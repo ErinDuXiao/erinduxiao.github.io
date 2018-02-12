7d044a313c47f0399341520548d484ed75e7f688
ErinDuXiao <suikatei@gmail.com>
2018-02-11 18:15:20 -0800

    first commit

---
+++ js/App.js
@@ -0,0 +1,31 @@
+/**
+ * Profile page controller
+ *
+ * @copyright: Erin Du
+ * @author:    Erin Du
+ * @version:   1.0.0
+ */
+ 'use strict';
+
+class App {
+
+    constructor() {
+        setUpHandlers();
+	}
+
+    run() {
+
+    }
+
+    setUpHandlers() {
+
+    }
+
+}
+
+
+// MAIN Entry point
+$(document).ready( function() {
+    let app = new App();
+    app.run();
+});
